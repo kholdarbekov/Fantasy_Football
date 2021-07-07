dc795603f19a2637014952f05e53d53dbeb399ee
kholdarbekov <international-2014@mail.ru>
2021-07-07 11:56:18 +0500

    auth: login and logout

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
-from .views import UsersListView, UserRegisterView
+from .views import UsersListView, UserRegisterView, UserLoginView, LogoutView
 
 urlpatterns = [
     path('user/register', UserRegisterView.as_view(), name='user_register'),
-    # path('user/login', UsersListView.as_view(), name='user_login'),
-    # path('user/logout', UsersListView.as_view(), name='user_logout'),
+    path('user/login', UserLoginView.as_view(), name='user_login'),
+    path('user/logout', LogoutView.as_view(), name='user_logout'),
 
     path('users/', UsersListView.as_view(), name='users_list'),
 
--- app/api/views.py
+++ app/api/views.py
@@ -1,16 +1,17 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, views
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from ..models import User
-from .serializers import UserSerializer, UserRegisterSerializer
+from .serializers import UserSerializer, UserRegisterSerializer, UserLoginSerializer
 
 
 class UsersListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated, ]
-    http_method_names = ['post']
+    http_method_names = ['post', ]
 
     def post(self, request, *args, **kwargs):
         users = User.objects.all()
@@ -22,7 +23,7 @@ class UsersListView(generics.ListAPIView):
 class UserRegisterView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
     permission_classes = [AllowAny, ]
-    http_method_names = ['post']
+    http_method_names = ['post', ]
 
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
@@ -34,3 +35,19 @@ class UserRegisterView(generics.CreateAPIView):
         headers = self.get_success_headers(serializer.data)
         token, created = Token.objects.get_or_create(user=serializer.instance)
         return Response({'token': token.key}, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class UserLoginView(ObtainAuthToken):
+    serializer_class = UserLoginSerializer
+    permission_classes = [AllowAny, ]
+    http_method_names = ['post', ]
+
+
+class LogoutView(views.APIView):
+    permission_classes = [IsAuthenticated, ]
+    http_method_names = ['post', ]
+
+    def post(self, request, *args, **kwargs):
+
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
